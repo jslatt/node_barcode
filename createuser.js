ae784814daa1e710cf381be7cdac5769bd343c4d
James Slattery <james@slattery.tech>
2017-04-12 20:45:19 -0700

    v1

--- createuser.js
+++ createuser.js
@@ -5,6 +5,16 @@ $('.CreateUser').click(function(){
 $('#barcode').focus();
 
 $('.searchBar').click(function(){
+  var code = $('#barcode').val();
+  db.user
+      .where("code").equalsIgnoreCase(code)
+      .each(function(code) {
+          $('.mainView').append('<div class="card"><div class="image"><img src="' + JSON.parse(JSON.stringify(code.img)) + '" alt="Picture" /></div><div class="content"><div class="header">' + JSON.parse(JSON.stringify(code.name)) + '</div><div class="meta"><a>' + JSON.parse(JSON.stringify(code.code)) + '</a></div><div class="description">' + JSON.parse(JSON.stringify(code.title)) + '</div></div><div class="extra content"><span class="right floated verified"></span><span><i class="user icon"></i>' + JSON.parse(JSON.stringify(code.id)) + '</span></div></div>');
+          if(code.status == true){
+            $('.verified').append('<i class="protect icon"></i>');
+          }
+      })
+
   $('#barcode').val(null);
   console.log("search");
   return false;
--- database.js
+++ database.js
@@ -2,31 +2,29 @@
 // Begin DB
 var db = new Dexie("UserDatabase");
 db.version(1).stores({
-    user: "++id,name,code,status"
+    user: "++id,name,code,img,title,status"
 });
 
 /*
 db.user.add({
     name: "James",
-    code: 001828,
+    code: "shalom",
+    img: "https://semantic-ui.com/images/avatar2/large/matthew.png",
+    title: "CEO",
     status: true
 })
 */
 //Add User function
-function AddUser(UserName,UserCode,UserStatus){
+function AddUser(UserName,UserCode,Uimg,Utitle,UserStatus){
   db.user.add({
     name: UserName,
     code: UserCode,
+    img: Uimg,
+    title: Utitle,
     status: UserStatus
   })
   return;
 };
 
-
-db.user
-    .where("name").equalsIgnoreCase("James")
-    .each(function(name) {
-        console.log(JSON.stringify(name));
-    })
     //.then();
 // End DB
